dde747e90a39cf5f2a90fed781859eb7c97606e7
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2021-02-13 16:00:21 +0000

    Criando a página com soma de inteiros

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+
+var Calculadora = {
+    // Calcula a soma de dois números
+    somar: function (num1, num2) {
+
+        let x = num1
+        let y = num2
+        let soma = x + y
+
+        return soma
+    },
+    subtracao: function(x, y) {
+        return x - y
+    },
+    potencia: function(x,y) {
+        return x ^ y
+    }
+}
